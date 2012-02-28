ea0d9b7a7d43d12d0e49b67e51cdf79bccb883fc
Adam Malcontenti-Wilson <adman.com@gmail.com>
2012-02-28 16:52:41 +1100

    Add feed accessor to Category objects

--- SbsOnDemand/Category.py
+++ SbsOnDemand/Category.py
@@ -1,6 +1,8 @@
 ## @namespace SbsOnDemand::Category
 # Module for managing video categories
 
+import config
+
 ## Represents a category
 class Category(object):
     '''
@@ -18,3 +20,11 @@ class Category(object):
         self.name = params.get('media$name',None)
         self.scheme = params.get('media$scheme',None)
     
+    ## Gets a video feed of all videos in that category
+    # @param feedId the feed unique identifier 
+    def getFeed(self,feedId = config.ALLDATA_FEEDID):
+        from Feed import Feed
+        return Feed({"feedId": feedId, "filter": {"byCategories": self.name}})
+
+    ## @see getFeed
+    feed = property(getFeed)
